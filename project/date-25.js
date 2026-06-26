fe18cc423635bce8f0b0e11cd8511602d0644b2e
krrish <krrishvirpariya51@gmail.com>
2026-06-26 10:02:32 +0530

    add to Category-Quantity-Out of Stock

--- project/date-25.js
+++ project/date-25.js
@@ -1,71 +1,91 @@
 let books = [
 
-{id:1,
+{
+id:1,
 name:"Gujarati Vyakaran",
 author:"Patel",
+category:"Education",
+quantity:5,
 status:"Available"
 },
 
-{id:2,
+{
+id:2,
 name:"Hindi Vyakaran",
 author:"Sharma",
-status:"Issued"
+category:"Language",
+quantity:0,
+status:"Out of Stock"
 },
 
-{id:3,
-name:"Gujarati Sahitya",
+{
+id:3,
+name:"Gujarati Vyakaran",
 author:"Joshi",
-status:"Issued"
-},
-
-{id:4,
-name:"Bal Varta",
-author:"Nayak",
+category:"Language",
+quantity:2,
 status:"Available"
 },
 
-{id:5,
+{
+id:4,
+name:"Bal Varta",
+author:"Nayak",
+category:"Story",
+quantity:4,
+status:"Available"
+},
+
+{
+id:5,
 name:"Mahabharat",
 author:"Vyas",
-status:"Issued"
+category:"Religious",
+quantity:0,
+status:"Out of Stock"
 }
-
 
 ];
 
 // console.log(books);
 
-
 document.querySelector(".display").onclick=function(){
 document.querySelector("table").classList.add("show");
 document.querySelector("tbody").innerHTML="";
 for(const book of books){
+book.status=(book.quantity>0)?"Available":"Out of Stock";
 document.querySelector("tbody").innerHTML+=`
 <tr>
 <td>${book.id}</td>
 <td>${book.name}</td>
 <td>${book.author}</td>
+<td>${book.category}</td>
+<td>${book.quantity}</td>
 <td>${book.status}</td>
-</tr>`;}
+</tr>`;
 }
-
+}
 
 document.querySelector(".add").onclick=function(){
 var book={
 id:Number(prompt("Enter Book ID : ")),
 name:prompt("Enter Book Name : "),
 author:prompt("Enter Author Name : "),
-status:"Available"};
+category:prompt("Enter Category : "),
+quantity:Number(prompt("Enter Quantity : "))};
+book.status=(book.quantity>0)?"Available":"Out of Stock";
 books.push(book);
 console.log(books);
 }
-
 
 document.querySelector(".issue").onclick=function(){
 var id=Number(prompt("Enter Book ID : "));
 for(const book of books){
 if(book.id==id){
-book.status="Issued";}
+if(book.quantity>0){
+book.quantity--;}
+book.status=(book.quantity>0)?"Available":"Out of Stock";
+}
 }
 }
 
@@ -74,7 +94,9 @@ document.querySelector(".return").onclick=function(){
 var id=Number(prompt("Enter Book ID : "));
 for(const book of books){
 if(book.id==id){
-book.status="Available";}
+book.quantity++;
+book.status=(book.quantity>0)?"Available":"Out of Stock";
+}
 }
 }
 
@@ -90,17 +112,17 @@ document.querySelector(".available").onclick=function(){
 document.querySelector("table").classList.add("show");
 document.querySelector("tbody").innerHTML="";
 for(const book of books){
-if(book.status=="Available"){
+book.status=(book.quantity>0)?"Available":"Out of Stock";
 document.querySelector("tbody").innerHTML+=`
 <tr>
 <td>${book.id}</td>
 <td>${book.name}</td>
 <td>${book.author}</td>
+<td>${book.category}</td>
+<td>${book.quantity}</td>
 <td>${book.status}</td>
 </tr>`;
 }
 }
-}
-
 
 document.querySelector("main").innerHTML+=books.length;
